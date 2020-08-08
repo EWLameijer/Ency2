3e8e4cccad735aa8cc96e3d32c1478938fdfe1b6
EWLameijer <eric_wubbo@hotmail.com>
2020-08-08 18:48:52 +0200

    Now clears term box and brings cursor back to termbox on escape, making it easier to edit new text.

--- main.js
+++ main.js
@@ -28,11 +28,29 @@ function caseInsensitive(f) {
     return (a, b) => f(a.toLocaleLowerCase(), b.toLocaleLowerCase());
 }
 
-const caseIndependentSort = (a,b) => a.toLowerCase().localeCompare(b.toLowerCase());
+const caseIndependentSort = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
+
+const sortedKeys = () => Object.keys(entries).sort(caseIndependentSort);
+
+// adjusted from https://stackoverflow.com/questions/3369593/how-to-detect-escape-key-press-with-pure-js-or-jquery
+document.onkeydown = function (evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        showNewEntry("", true);
+        enteredTerm.focus();
+    }
+};
 
 // START THE ACTUAL WORK!
 
 output.innerText = data.toString()
+
 
 analyze(data.toString())
 
@@ -40,17 +58,15 @@ function analyze(str) {
     document.getElementById("numEntries").innerHTML = Object.keys(entries).length; // may want to update this, but saving is more important!
     const keys = Object.keys(entries);
     outputField.innerText = keys.join("\n");
-    const sortedKeys = keys.sort(caseIndependentSort);
-    const firstTerm = sortedKeys[0];
-    showNewEntry(firstTerm, sortedKeys, true);
+    const firstTerm = sortedKeys()[0];
+    showNewEntry(firstTerm, true);
 }
 
 function changeTerm() {
     const termGivenByUser = enteredTerm.value;
-    const sortedKeys = Object.keys(entries).sort(caseIndependentSort);
-    const selectedTerms = (termGivenByUser !== "") ? sortedKeys.filter(term => caseInsensitive(startsWith)(term, termGivenByUser)) : [];
+    const selectedTerms = (termGivenByUser !== "") ? sortedKeys().filter(term => caseInsensitive(startsWith)(term, termGivenByUser)) : [];
     const firstTerm = (selectedTerms.length > 0) ? selectedTerms[0] : termGivenByUser;
-    showNewEntry(firstTerm, sortedKeys);
+    showNewEntry(firstTerm);
 }
 
 function updateDescription() {
@@ -99,19 +115,18 @@ function considerScrolling() {
     const KEYCODE_DOWN = 40;
     const KEYCODE_UP = 38;
     const selectedTerm = focusedTermLabel.textContent ?? "";
-    const sortedKeys = Object.keys(entries).sort(caseIndependentSort);
     if (keyCode === KEYCODE_DOWN) {
-        const newTerm = sortedKeys.find(term => caseInsensitive(larger)(term, selectedTerm));
-        if (newTerm) showNewEntry(newTerm, sortedKeys, true);
+        const newTerm = sortedKeys().find(term => caseInsensitive(larger)(term, selectedTerm));
+        if (newTerm) showNewEntry(newTerm, true);
     } else if (keyCode === KEYCODE_UP) {
-        const newTerm = sortedKeys.reverse().find(term => caseInsensitive(smaller)(term, selectedTerm));
-        if (newTerm) showNewEntry(newTerm, sortedKeys, true);
+        const newTerm = sortedKeys().reverse().find(term => caseInsensitive(smaller)(term, selectedTerm));
+        if (newTerm) showNewEntry(newTerm, true);
     }
 }
 
-function showNewEntry(newTerm, sortedKeys, updateTermbox = false) {
+function showNewEntry(newTerm, updateTermbox = false) {
     if (updateTermbox) enteredTerm.value = newTerm;
-    outputField.innerText = sortedKeys.filter(term => caseInsensitive(larger)(term, newTerm)).join("\n");
+    outputField.innerText = sortedKeys().filter(term => caseInsensitive(larger)(term, newTerm)).join("\n");
     const description = entries[newTerm] ?? "";
     focusedTermLabel.innerHTML = `<i>${newTerm}</i>`;
     descriptionArea.value = description;
